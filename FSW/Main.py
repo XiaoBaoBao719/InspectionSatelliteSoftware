1a49103fa400d7d313a7c6d651125e2d5202dd1c
XiaoBaoBao719 <xiao-bao719@hotmail.com>
2021-12-26 00:50:11 -0800

    main

---
+++ FSW/Main.py
@@ -0,0 +1,25 @@
+""" Main Flight Software Class
+@Author: Xiao-Bao Bao
+@License: GNU Commons
+
+This file forms the basis of the InspectionCV primary payload and shall be
+invoked at a predetermined schedule buy the flight computer running Debian 11
+on an ARMx64 core processor. Primary functions include: 
+1. 
+2. 
+3.
+
+"""
+
+from time import time
+import numpy as np
+import loguru
+from loguru import logger
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
