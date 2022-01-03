8778db0bdef303aaf7f8ee638dedc16931efa650
Xiao Bao Bao <xiao-bao719@hotmail.com>
2022-01-03 11:30:56 -0800

    Add files via upload
    TODO: Add more documentation!

--- FSW/Main.py
+++ FSW/Main.py
@@ -8,45 +8,73 @@
 This is the master file for the main function of the InspectionCV primary payload
 and performs the needed schedule function calls for the flight computer's data collection.
 Primary functions include: 
-1. 
-2. 
-3.
-
+1. Payload deployment
+2. Invoking model inference results
+3. Serial data packet comm to the S/C FC
 """
+# =====================================
+# ==         CONFIGURATION           ==
+# =====================================
+
 from os import *
 from time import time
+from loguru import logger
+
 import numpy as np
 import math
 import loguru
-from loguru import logger
 import Burnwire as bw
 import RPi.GPIO as GPIO
 import serial
+import threading
 
-# Global variables
+# =====================================
+# ==         DEPLOYMENT VARS         ==
+# =====================================
 PHOTODIODE_PIN = 9
 INPUT_PIN = 10
 BURN_PIN_1 = 11
 BURN_PIN_2 = 12
 LED_PIN = 14
-
 BURNWIRE_PINS = (BURN_PIN_1, BURN_PIN_2)
 BURNWIRE_WAIT_TIME = 20 # mins
-
 LUMINOSITY_THRESHOLD = 1
-SCIENCE = False
 
-MAX_TEMP=85 # measured in degrees C (see datasheet -20C - +85C)
-
-BAUDRATE=9600
-SYS_TIMEOUT=5
-SERIAL_PORT='/dev/ttyS0'
+# =====================================
+# ==       COMMS GLOBAL VARS         ==
+# =====================================
+BAUDRATE = 9600
+SYS_TIMEOUT = 5
+mini_UART = '/dev/ttyS0'
+PL011 = '/dev/ttyAMA0'
+SERIAL_PORT = PL011
 ser=serial.Serial()
 
+# =====================================
+# ==        MISC GLOBAL VARS         ==
+# =====================================
+SCIENCE = False
+MAX_TEMP = 85 # measured in degrees C (see datasheet -20C - +85C)
+
+
 def startTimer():
+    """ TODO: initializes the global timer var as a seperate thread process
+    """
     pass
 
 def checkDeployed():
+    """ Determines if the payload mechanism has been triggered and deployed by
+        comparing ambient light to some static threshold value
+        
+    Parameters
+    ----------
+    val: float
+        Contains the digital photodiode readings
+    
+    Returns
+    ---------
+    boolean output based on if deploy criterion met
+    """
     val = GPIO.input(PHOTODIODE_PIN)
     if val < LUMINOSITY_THRESHOLD:
         return False
@@ -54,12 +82,18 @@ def checkDeployed():
         return True
 
 def initializeComputer():
+    """ Setup calls to RPi.GPIO to initialize pin numbers to board specs and starts
+        the experiment timer
+    """
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
     GPIO.setup(LED_PIN, GPIO.OUT)
     startTimer()
         
 def initializeBurnwire():
+    """ Setup calls to RPi.GPIO to initialize pin numbers to board specs and assigns
+        photodiode to a pulldown resistor
+    """
     GPIO.setup(BURN_PIN_1, GPIO.OUT, initial = GPIO.LOW)
     GPIO.setup(BURN_PIN_2, GPIO.OUT, initial = GPIO.LOW)
     GPIO.setup(INPUT_PIN, GPIO.IN)
@@ -67,6 +101,18 @@ def initializeBurnwire():
     GPIO.cleanup()
     
 def fireBurnwire():
+    """ Calls GPIO pins in order to run current through burn resistor and
+        trigger the release mechanism
+        
+    Parameters
+    ----------
+    val: float
+        Contains the digital photodiode readings
+    
+    Returns
+    ---------
+    
+    """
     # Immediately perform burnwire deploy for 5 seconds
     GPIO.output(burnChanList, GPIO.HIGH)
     prtStatus = GPIO.output(burnChanList, not GPIO.input(burnChanList))
@@ -76,7 +122,7 @@ def fireBurnwire():
     prtStatus += GPIO.output(burnChanList, not GPIO.input(burnChanList)) # Report to sys diagnostic
     time.sleep(10) # Wait 10 seconds
 
-def systemSetup():
+def serialSetup():
     SCIENCE = True
     # Initialize Serial Port
     ser = serial.Serial(port=SERIAL_PORT, baudrate=BAUDRATE,parity=Serial.PARITY_EVEN,
@@ -84,7 +130,7 @@ def systemSetup():
                             bytesize=serial.EIGHTBITS)
     counter = 0
     print(ser.name)
-    ser.write(b'CV_Payload_Activated')
+    ser.write(b'CV_Payload_Active')
     ser.open()
     
 
@@ -129,7 +175,7 @@ def writePayloadData(results):
 
 def setup():
     initializeComputer()
-    systemSetup()
+    serialSetup()
     initializeBurnwire()
     
     while !checkDeployed():
