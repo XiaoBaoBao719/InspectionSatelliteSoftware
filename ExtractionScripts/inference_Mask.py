30edc63411e11436ea65b3e2b55ae88526f6a31d
bom16 <womazotti@ucdavis.edu>
2022-01-29 20:18:32 -0800

    Updates to inference_Mask.py
    Ready for libraries and modules to be added. dmyModel.pth was not pushed due to file size.

--- ExtractionScripts/inference_Mask.py
+++ ExtractionScripts/inference_Mask.py
@@ -4,33 +4,46 @@ Runs Mask model and returns detection data for further analysis
 
 Input [type XX]: captured image
 Output [type dict]: Dictionary of detections containing detection bounding 
-        box coordinates and detection confidence ; have ordered s.t. 
+        box coordinates and detection confidence; have ordered s.t. 
         detections[0] has highest confidence and detections[-1] has 
         lowest confidence 
 
-Author(s): 
+Author(s): Billy Orion Mazotti
 
 Last Edited: 1/29/22
-
 """
 
-# check pytorch installation: 
-import torch, torchvision
-print(torch.__version__, torch.cuda.is_available())
-assert torch.__version__.startswith("1.9")   # please manually install torch 1.9 if Colab changes its default version
+# imports
+""" [NEEDS DEPENDENCIES SATISFIED --> @XB]
+"""
+import cv2
 
-# Setup detectron2 logger
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
+ 
+# load pre-trained Mask R-CNN model (based on colab work)
+""" [NEEDS .pth MODEL FILE --> @YC]
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "[MODEL_FILE.pth]")  # path to the model we just trained
+"""
 
-# import some common libraries
-import numpy as np
-import os, json, cv2, random
+# inference the image (based on colab work)
+""" [UNCOMMENT AFTER ADDING DEPENDENCIES AND MODEL.pth]
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
 
-# import some common detectron2 utilities
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
+
+# create dicitonary of detections made (based on colab work)
+# format --> {0: {'bbox': [3690.1355, 398.60562, 3997.3171, 568.5226], 'conf': 1.0, 1:...}
+detect_bbox = outputs["instances"].pred_boxes.tensor.cpu().numpy()
+detect_conf = outputs["instances"].scores.cpu().numpy()
+num_detect = len(outputs["instances"].pred_boxes.tensor)
+
+# gather bbox coordinates and confidence for each deteciton per image ((based on colab work))
+det_dict = {}
+for i in range(num_detect):
+  det_dict[i] = {"bbox":{}, "conf":{}}
+  det_dict[i]["bbox"] = list(detect_bbox[i])
+  det_dict[i]["conf"] = round(detect_conf[i],2)
+"""
+
+im = cv2.imread("dmyImgCap.jpg")
+print(type(im))
