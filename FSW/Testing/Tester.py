42e2b6b6a2a1accb3668f12a5d3616a77ce8970b
bom16 <womazotti@ucdavis.edu>
2022-01-29 17:47:56 -0800

    Adding Science FSW Architecture

--- FSW/Testing/Tester.py
+++ FSW/Testing/Tester.py
@@ -1,10 +1,3 @@
-import sys
-sys.path.append('/home/xiaobao/InspectionSatCV/FSW/')
-
-import TestBuddy
-import Sub_Tests.SomeMoreTests as sub
-
-TestBuddy.one_test() # Adjcent dir
-
-sub.sub_tests() # Testing sub directories
-
+test = "temp=61.8'C"
+out = float(test[5:9])
+print(out)
