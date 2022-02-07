e8ea1cae31c130febef1adee1b628408ca8a37dd
bom16 <womazotti@ucdavis.edu>
2022-02-06 17:38:45 -0800

    Update inference_Mask.py
    Verified this works on Co-Lab, may have trouble with dependencies on RasPi

--- ExtractionScripts/inference_Mask.py
+++ ExtractionScripts/inference_Mask.py
@@ -23,54 +23,22 @@ Functions that need library: get_cfg(), DefaultPredictor(), cv2.imread()
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 import cv2
-
 """
-
-# load pre-trained Mask R-CNN model 
-# BASED ON COLAB --> MAKE RASPI/UPBOARD FRIENDLY
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_0004999.pth")  # path to the model we just trained
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-
-# inference the image 
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.02   # set a custom testing threshold
-predictor = DefaultPredictor(cfg)
-im = cv2.imread("capImgAcc.jpg")
-
-
-outputs = predictor(im)       #dict, colab format --> {'instances': Instances(..., fields =[pred_boxes:..., scores:...])}
-# print(outputs)
-
-# create dicitonary of detections made (based on colab work, may need changing if )
-# format --> {0: {'bbox': [3690.1355, 398.60562, 3997.3171, 568.5226], 'conf': 1.0, 1:...}
-detect_bbox = outputs["instances"].pred_boxes.tensor.cpu().numpy()
-detect_conf = outputs["instances"].scores.cpu().numpy()
-num_detect = len(outputs["instances"].pred_boxes.tensor)
-
-# gather bbox coordinates and confidence for each deteciton per image ((based on colab work))
-det_dict = {}
-for i in range(num_detect):
-  det_dict[i] = {"bbox":{}, "conf":{}}
-  det_dict[i]["bbox"] = list(detect_bbox[i])
-  det_dict[i]["conf"] = round(detect_conf[i],2)
 
 
 # # load pre-trained Mask R-CNN model 
 # # BASED ON COLAB --> MAKE RASPI/UPBOARD FRIENDLY
 # cfg = get_cfg()
 # cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-# cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "dmyModle.pth")  # path to the model we just trained
+# cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_0004999.pth")  # path to the model we just trained
 # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
 # os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
-
 # # inference the image 
 # # BASED ON COLAB --> MAKE RASPI/UPBOARD FRIENDLY
-# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set a custom testing threshold
+# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.02   # set a custom testing threshold
 # predictor = DefaultPredictor(cfg)
-# im = cv2.imread("dmyImgCap.jpg")
+# im = cv2.imread("capImgAcc.jpg")
 
 # outputs = predictor(im)       #dict, colab format --> {'instances': Instances(..., fields =[pred_boxes:..., scores:...])}
 
