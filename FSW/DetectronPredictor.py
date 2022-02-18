82682d97080692f4b49f45b74739ceca7e6f9467
XiaoBaoBao719 <xiao-bao719@hotmail.com>
2022-02-17 22:42:02 -0800

    minor changes

--- FSW/DetectronPredictor.py
+++ FSW/DetectronPredictor.py
@@ -17,7 +17,6 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
-
 from detectron2.structures import BoxMode
 
 
